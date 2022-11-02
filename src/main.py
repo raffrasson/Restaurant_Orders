25477040b737b1352f22f144c6b7384a77f1e787
raffrasson <raf.frasson@gmail.com>
2022-11-02 16:35:51 -0300

    feat: completes req. 2

--- src/main.py
+++ src/main.py
@@ -5,14 +5,14 @@ from track_orders import TrackOrders
 
 
 def print_info(tracker, control):
-    print(tracker.get_most_ordered_dish_per_customer('maria'))
-    print(tracker.get_never_ordered_per_customer('joao'))
-    print(tracker.get_days_never_visited_per_customer('joao'))
+    print(tracker.get_most_ordered_dish_per_customer("maria"))
+    print(tracker.get_never_ordered_per_customer("joao"))
+    print(tracker.get_days_never_visited_per_customer("joao"))
     print(control.get_quantities_to_buy())
 
 
 def main():
-    topic = 'order'
+    topic = "order"
     path = ""
 
     tracker = TrackOrders()
@@ -23,7 +23,7 @@ def main():
         pub.subscribe(sub, topic)
 
     with open(path) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
+        csv_reader = csv.reader(csv_file, delimiter=",")
         for customer, order, day in csv_reader:
             pub.sendMessage(topic, customer=customer, order=order, day=day)
 
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+
 class TrackOrders:
     def __init__(self):
         self.data = []
@@ -6,19 +9,63 @@ class TrackOrders:
         return len(self.data)
 
     def add_new_order(self, customer, order, day):
-        self.data.append({customer, order, day})
+        self.data.append((customer, order, day))
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        customer_orders = []
+        all_orders = list(self.data)
+
+        for order in all_orders:
+            if order[0] == customer:
+                customer_orders.append(order[1])
+        return Counter(customer_orders).most_common(1)[0][0]
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        ordered_dishes = []
+        unordered = []
+        all_orders = list(self.data)
+        for order in all_orders:
+            if order[0] == customer:
+                ordered_dishes.append(order[1])
+            else:
+                unordered.append(order[1])
+        for order in all_orders:
+            if order[0] == customer:
+                ordered_dishes.append(order[1])
+            else:
+                unordered.append(order[1])
+        return set(unordered) - set(ordered_dishes)
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        days_visited = []
+        not_visited = []
+        all_orders = list(self.data)
+        for order in all_orders:
+            if order[0] == customer:
+                days_visited.append(order[2])
+            else:
+                not_visited.append(order[2])
+        for order in all_orders:
+            if order[0] == customer:
+                days_visited.append(order[2])
+            else:
+                not_visited.append(order[2])
+        return set(not_visited) - set(days_visited)
 
     def get_busiest_day(self):
-        pass
+        days = []
+        all_orders = list(self.data)
+
+        for order in all_orders:
+            days.append(order[2])
+        print(days)
+        return Counter(days).most_common(1)[0][0]
 
     def get_least_busy_day(self):
-        pass
+        days = []
+        all_orders = list(self.data)
+
+        for order in all_orders:
+            days.append(order[2])
+        print(days)
+        return Counter(days).most_common()[-1][0]
